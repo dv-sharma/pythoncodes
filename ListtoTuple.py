7fc7cc3aba226d875236bdd6ef9142aa64a01794
dv-sharma <divyam.sharma3@gmail.com>
2023-10-23 14:55:01 -0400

    coding practise problem 1

---
+++ ListtoTuple.py
@@ -0,0 +1,8 @@
+# Write your code here
+my_list = [34, 82.6, "Darth Vader", 17, "Hannibal"]
+var_1 = my_list[0]
+var_2 = my_list.pop()
+var_3 = len(my_list)+1
+new_list= [var_1,var_2,var_3]
+my_tuple = tuple(new_list)
+print(my_tuple)
